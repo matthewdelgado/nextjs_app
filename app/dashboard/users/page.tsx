78d621c8e8a9adcde4e6f8afa8cd3c83f1830b01
Matthew Delgado <matthewdelgado.swe@gmail.com>
2025-11-07 17:36:03 -0500

    add dynamic routing

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div>
+      <h1>Users Dashboard</h1>
+      <ul>
+        <li>
+          <Link href="/dashboard/users/1">User 1</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/2">User 2</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/3">User 3</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/4">User 4</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Users;
