b18bc713abed0d3d27410b71ebb94d900c2498d3
Matthew Delgado <matthewdelgado.swe@gmail.com>
2025-11-07 17:39:25 -0500

    improve dynamic routing

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,5 +1,10 @@
-const UserDetails = () => {
-  return <div>UserDetails</div>;
+const UserDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  return (
+    <div>
+      <h1>Showing details for user #{id}</h1>
+    </div>
+  );
 };
 
 export default UserDetails;
