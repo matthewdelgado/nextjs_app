5333e8066ec1b6f442d2971e7b957109ee706d12
Matthew Delgado <matthewdelgado.swe@gmail.com>
2025-11-08 16:11:59 -0500

    add general styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Hello from "@/components/hello";
+import ExploreBtn from "@/components/ExploreBtn";
 
 const Home = () => {
   return (
-    <main>
-      <div>Welcome to Next.js</div>
-      <Hello />
-    </main>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev Event You Can't Miss</h1>
+      <p className="text-center mt-5">Hackathons, meetups, and conferences, all in one place</p>
+      <ExploreBtn />
+    </section>
   );
 };
 
