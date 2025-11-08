bad737be681db08c3a407318a0f7e4bf242c6001
Matthew Delgado <matthewdelgado.swe@gmail.com>
2025-11-08 16:16:35 -0500

    add feature events placeholder to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,14 @@ const Home = () => {
       <h1 className="text-center">The Hub for Every Dev Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, meetups, and conferences, all in one place</p>
       <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3> Feature Events</h3>
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
